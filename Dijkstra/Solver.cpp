af8efac3eeb76047a11c2e915ce4dbb53a7e1240
JasonBK <jasonkitoko@hotmail.com>
2026-08-03 22:50:49 -0400

    added pseudocode to  solver,,,to be continued

--- Dijkstra/Solver.cpp
+++ Dijkstra/Solver.cpp
@@ -1,1 +1,21 @@
 #include "Solver.h"
+
+//update estimates
+    //update estimated min distance to connected vertices
+        // for all neighbours of current node
+        // if it is unexplored
+            //calc distance
+                // add cost to current node node + cost to traverse the new node 
+            //        
+        // CHECK stored DISTANCE to node
+            // compare stored vs current distance
+                //if stored >current min, save current distance as node and current node as previous node
+    //mark current node as explored
+//choose next vertex
+    //choose unexplored node with smallest estimate
+        //pick smallest vallue from unexplored nodes list
+        // current node = min nod
+
+//functions find all neighbours ocurrent node
+    // make sure all nodes unexplored
+        //
--- Dijkstra/Solver.h
+++ Dijkstra/Solver.h
@@ -7,9 +7,9 @@ class Solver
 private:
 	std::list<Node> visitedNodes;
 	std::list<Node> unvisitedNodes;
+// map with node ,cost to go to node, previous node,
 public:
 	void markNodeVisited();
 	void visitNode();
 	void updatenodes();
 };
-
